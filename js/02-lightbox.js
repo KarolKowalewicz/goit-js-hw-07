811831b53e4a127a7a0bb138b557d12f0e5fb4b1
KarolKowalewicz <kowalewiczkarol@gmail.com>
2023-03-05 19:05:40 +0100

    chyba juz

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+// Change code below this line
+
+const galleryContainer = document.querySelector(".gallery");
+
+const galleryMarkup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+      <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+      </a>
+    `;
+  })
+  .join("");
+
+galleryContainer.insertAdjacentHTML("beforeend", galleryMarkup);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionDelay: 250,
+});
